29feb5d7aa21fb560bc4ad29b005ababe09a8b96
Ali Shazal <alishazal@172-18-25-225.DYNAPOOL.NYU.EDU>
2018-11-14 22:58:37 -0500

    Added register using passport

--- src/app.js
+++ src/app.js
@@ -5,37 +5,37 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const publicPath = path.resolve(__dirname, 'public');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const User = mongoose.model('User');
 const Expense = mongoose.model('Expense');
+const Friend = mongoose.model('Expense');
 const session = require('express-session');
 const sessionOptions = {
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
+    secret: 'this is a secret',
+    resave: false,
+    saveUninitialized: false
 };
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(publicPath));
 app.use(session(sessionOptions));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
-// homepage
-app.get('/login', (req, res) => {
-    res.render('login');
-});
-
-app.get('/register', (req, res) => {
-    res.render('register');
-});
 
 app.get('/', (req, res) => {
     res.redirect('/home');
 });
 
 app.get('/home', (req, res) => {
-    res.render('home');
+    res.render('index');
 });
 
 app.get('/home/addExpense', (req, res) => {
@@ -43,19 +43,50 @@ app.get('/home/addExpense', (req, res) => {
 });
 
 app.get('/home/balance', (req, res) => {
-    res.render('index');
+    res.render('balance');
 });
 
 app.get('/home/settle', (req, res) => {
-    res.render('index');
+    res.render('settle');
 });
 
 app.get('/home/addFriend', (req, res) => {
-    res.render('addExpense');
+    res.render('addFriend');
 });
 
 app.get('/home/friends', (req, res) => {
-    res.render('addExpense');
+    res.render('friends');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+}); 
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+    });
+
+    User.register(newUser, req.body.password, (err, user) => {
+        if (err){
+            console.log(err);
+            return res.render('register', {message: err});
+        }
+        passport.authenticate("local")(req, res, () => {
+            res.redirect('/');
+        });
+    });
+}); 
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    
 });
 
 app.get('*', function (req, res) {
--- src/db.js
+++ src/db.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
-const User = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true }, 
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true}
+    password: { type: String}
 });
 
-const Expense = new mongoose.Schema({
+const expenseSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     description: { type: String, required: true },
     totalAmount: { type: Number, min: 0.01, required: true },
@@ -17,15 +18,17 @@ const Expense = new mongoose.Schema({
     createdAt: { type: Date, required: true },
 });
 
-const Friend = new mongoose.Schema({
+const friendSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true }, 
 });
 
-mongoose.model("User", User);
-mongoose.model("Expense", Expense);
-mongoose.model("Friend", Expense);
+userSchema.plugin(passportLocalMongoose);
+
+mongoose.model("User", userSchema);
+mongoose.model("Expense", expenseSchema);
+mongoose.model("Friend", friendSchema);
 
 mongoose.connect("mongodb://localhost/finalProject", { useNewUrlParser: true });
