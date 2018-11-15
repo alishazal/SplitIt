e2ac6ff88f3893d08a0815e21f2a0ffba4a75099
Ali Shazal <alishazal@172-18-25-225.DYNAPOOL.NYU.EDU>
2018-11-14 22:09:59 -0500

    Updated db schemas. Added login & register hbs files.

--- src/app.js
+++ src/app.js
@@ -1,40 +1,65 @@
 require('./db');
-
-// enable express
 const express = require("express");
 const app = express();
-
-// enable body parser
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// enable static files
 const path = require('path');
 const publicPath = path.resolve(__dirname, 'public');
-app.use(express.static(publicPath));
-
-// db
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const Expense = mongoose.model('Expense');
-
-
-// enable sessions
 const session = require('express-session');
 const sessionOptions = {
-    secret: 'secret for signing',
+    secret: 'secret',
     resave: true,
     saveUninitialized: true
 };
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(publicPath));
 app.use(session(sessionOptions));
 
-// view template
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // homepage
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
 app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+
+app.get('/home', (req, res) => {
+    res.render('home');
+});
+
+app.get('/home/addExpense', (req, res) => {
+    res.render('addExpense');
+});
+
+app.get('/home/balance', (req, res) => {
     res.render('index');
+});
+
+app.get('/home/settle', (req, res) => {
+    res.render('index');
+});
+
+app.get('/home/addFriend', (req, res) => {
+    res.render('addExpense');
+});
+
+app.get('/home/friends', (req, res) => {
+    res.render('addExpense');
+});
+
+app.get('*', function (req, res) {
+    res.render("404page");
 });
 
 app.listen(3000);
--- src/db.js
+++ src/db.js
@@ -1,24 +1,31 @@
 const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
-    username: { type: String, required: true, unique: true},
-    password: { type: String, required: true},
-    email: { type: String, required: true },
-    fullName: { type: String, required: true },
-    expenses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Expense' }]
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true }, 
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true}
 });
 
 const Expense = new mongoose.Schema({
-    username: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     description: { type: String, required: true },
     totalAmount: { type: Number, min: 0.01, required: true },
-    iPaid: { type: Boolean, required: true },
-    splitWays: { type: Number, min: 1, required: true },
+    paidBy: { type: String, required: true },
     splitBetween: { type: Array, required: true },
     createdAt: { type: Date, required: true },
 });
 
+const Friend = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true }, 
+});
+
 mongoose.model("User", User);
 mongoose.model("Expense", Expense);
+mongoose.model("Friend", Expense);
 
 mongoose.connect("mongodb://localhost/finalProject", { useNewUrlParser: true });
